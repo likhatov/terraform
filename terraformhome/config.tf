addb415a0c26fd8c63fcf74bc4de4e6df5af4a6f
Pavel <lpv.mail@gmail.com>
2020-03-19 08:52:32 +0000

    TF final

--- terraformhome/config.tf
+++ terraformhome/config.tf
@@ -5,13 +5,32 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami = "ami-0d1cd67c26f5fca19"
   instance_type = "t2.micro"
+  security_groups = ["allow-all"]
+  key_name = "aws-home"  
+  
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file("/home/lpv_mail/aws-home.pem")}"
+    agent = false
+  }
 
-  provisioner "local-exec" {
-    command = "apt-get update && apt-get install nginx -y"
-}
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install nginx -y"
+    ]
+  }
+
   provisioner "file" {
     source = "index.html"
-    destination = "/var/www/html/"
-}
+    destination = "/tmp/index.html"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo mv /tmp/index.html /var/www/html/"]
+  }
+
 }
 
